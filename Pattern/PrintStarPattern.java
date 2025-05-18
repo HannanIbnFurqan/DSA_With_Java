b6c063389f822137ee286dc832959c297243e6d8
Mohd Hannan <mohdhannan7913@gmail.com>
2025-05-18 15:27:49 +0530

    PrintStarPattern

---
+++ Pattern/PrintStarPattern.java
@@ -0,0 +1,13 @@
+package Pattern;
+
+public class PrintStarPattern {
+    public static void main(String[] args) {
+        for(int line = 1; line <= 5; line++){
+            for(int j = 1; j <= line; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+    
+}
