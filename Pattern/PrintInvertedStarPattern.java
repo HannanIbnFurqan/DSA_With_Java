ef9eaf95949da673b0d35a68ee7baa0cc25d5ed8
Mohd Hannan <mohdhannan7913@gmail.com>
2025-05-19 23:57:40 +0530

    PrintInvertedStarPattern

--- Pattern/PrintInvertedStarPattern.java
+++ Pattern/PrintInvertedStarPattern.java
@@ -3,6 +3,12 @@ package Pattern;
 public class PrintInvertedStarPattern {
     public static void main(String[] args) {
         int n = 4;
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n-1+1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
         
     }
     
