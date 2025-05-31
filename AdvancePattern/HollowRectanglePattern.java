55b151c2a8937b79305fc538d280f14d601dc37b
Mohd Hannan <mohdhannan7913@gmail.com>
2025-05-31 23:44:23 +0530

    HollowRectanglePattern

---
+++ AdvancePattern/HollowRectanglePattern.java
@@ -0,0 +1,20 @@
+package AdvancePattern;
+
+public class HollowRectanglePattern {
+    public static void main(String[] args) {
+        int n = 4;
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= n; col++) {
+                if (row == 1 || row == n || col == 1 || col == n){
+                    System.out.print("*");
+
+                }else{
+                     System.out.print(" ");
+                }
+                
+
+            }
+            System.out.println();
+        }
+    }
+}
