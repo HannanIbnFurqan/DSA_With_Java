f7f7f952db7b368796df5084c5418a3c52b8a91b
Mohd Hannan <mohdhannan7913@gmail.com>
2025-06-03 23:26:42 +0530

    modified

--- Function/SumOfTwoNumberUsingFunction.java
+++ Function/SumOfTwoNumberUsingFunction.java
@@ -3,15 +3,26 @@ package Function;
 import java.util.Scanner;
 
 public class SumOfTwoNumberUsingFunction {
+
+    // Function to calculate and print sum
     public static void Calculate(int a, int b){
-        int sum = a+b;
-        System.out.println(sum);
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
     }
+
     public static void main(String[] args) {
-        Scanner  sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+
+        // Take input
+        System.out.print("Enter first number: ");
         int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
         int b = sc.nextInt();
+
+        // Call function
         Calculate(a, b);
+
+        sc.close(); // Good practice to close the scanner
     }
-    
 }
