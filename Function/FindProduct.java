e48f1a245d11eaaee0a6923b6164b4cbfd8a2cfe
Mohd Hannan <mohdhannan7913@gmail.com>
2025-06-03 23:22:59 +0530

    FindProduct

---
+++ Function/FindProduct.java
@@ -0,0 +1,14 @@
+package Function;
+
+public class FindProduct{
+    public static int multiple(int a, int b){
+        int product = a*b;
+        return product;
+    }
+    public static void main(String[] arg){
+         int P = multiple(10,10);
+         System.out.println(P);
+         
+
+    }
+}
