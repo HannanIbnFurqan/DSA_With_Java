e53caa0dfa37481f59f8c2ae4af7f1f79fe2f921
Mohd Hannan <mohdhannan7913@gmail.com>
2025-08-07 13:02:59 +0530

    arrayAsArgument

---
+++ Array/arrayAsArgument.java
@@ -0,0 +1,16 @@
+public class arrayAsArgument {
+    public static void update(int marks[]){
+         for(int i = 0; i<marks.length; i++){
+             marks[i] = marks[i]+1;
+         }
+    }
+    public static void main(String[] args) {
+        int marks[] = {10,20,30,40};
+        update(marks);
+
+        // check update number
+        for(int i = 0; i<marks.length; i++){
+            System.out.println(marks[i]);
+        }
+    }
+}
