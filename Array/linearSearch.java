f9f87ad85f8de3a7fbaf54db6cc2bebb7e44f9c3
Mohd Hannan <mohdhannan7913@gmail.com>
2025-08-09 00:19:27 +0530

    linearSearch

---
+++ Array/linearSearch.java
@@ -0,0 +1,23 @@
+public class linearSearch {
+    public static int checkIndex(int numbers[], int key) {
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == key) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50, 60};
+        int key = 50;
+
+        int index = checkIndex(numbers, key);
+
+        if (index == -1) {
+            System.out.println("Not Found");
+        } else {
+            System.out.println("Index is = " + index);
+        }
+    }
+}
