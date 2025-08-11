6043dc22893db469b0b7df2b19c5392a43f834e0
Mohd Hannan <mohdhannan7913@gmail.com>
2025-08-12 00:00:52 +0530

    solve error

--- Array/reverseArray.java
+++ Array/reverseArray.java
@@ -1,18 +1,21 @@
 public class reverseArray {
-    public static void reverse(int number[]){
+    public static void reverse(int number[]) {
         int first = 0;
-        int last = number.length-1;
+        int last = number.length - 1;
         while (first < last) {
             int temp = number[last];
-                number[last] = number[first];
-                number[first] = temp;
+            number[last] = number[first];
+            number[first] = temp;
+            first++;
+            last--;
         }
     }
+
     public static void main(String[] args) {
-        int number[] = {1,2,3,4,5,6,7,8,9,10};
+        int number[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
         reverse(number);
-        for(int i = 0; i <= number.length; i++){
-            System.out.println(number[i]);
+        for (int i = 0; i < number.length; i++) {
+            System.out.print(number[i] + " ");
         }
 
     }
